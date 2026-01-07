fa2c2ddef4d23917713392ba7a2ea243491988f3
Lucas Ringe <lucaswringe@gmail.com>
2026-01-07 00:26:36 +0000

    feature: added base mapviewport

---
+++ mortar-app-frontend/src/components/MapViewport.svelte
@@ -0,0 +1,34 @@
+<script lang='ts'>
+	// import type OpenSeadragon from 'openseadragon';
+    // import { onMount } from 'svelte';
+
+    // onMount(async () => {
+    //     // const OSD = (await import("openseadragon")).default;
+    //     // const viewer : OpenSeadragon.Viewer = new OSD.Viewer({
+    //     //         id : 'openseadragon-instance',
+    //     //         prefixUrl: "https://cdnjs.cloudflare.com/ajax/libs/openseadragon/4.1.0/images/",
+    //     //         tileSources: {
+    //     //             type: "image",
+    //     //             url: "/new_map.jpg",
+    //     //         },
+    //     //         showNavigationControl: false,
+    //     //         gestureSettingsMouse: { clickToZoom: false },
+    //     //         maxZoomLevel: 5,
+    //     //         springStiffness: 150,
+    //     //     });
+
+    //     // // Debug: Log if the image actually opens
+    //     // viewer.addHandler('open', () => {
+    //     //     console.log("OSD: Image opened successfully!");
+    //     // });
+
+    //     // viewer.addHandler('open-failed', (msg) => {
+    //     //     console.error("OSD: Image failed to load", msg);
+    //     // });
+    // });
+
+</script>
+
+<div class='bg-black w-screen h-screen'>
+    <!-- <div id='openseadragon-instance' class="w-full h-full bg-red-900"></div> -->
+</div>
--- mortar-app-frontend/src/routes/+page.svelte
+++ mortar-app-frontend/src/routes/+page.svelte
@@ -1,13 +1,14 @@
 <script>
 	import { getMortarState } from "$lib/mortar_state.svelte";
     import ControlPanel from "../components/ControlPanel.svelte";
+    import MapViewport from "../components/MapViewport.svelte";
 
     let mortar_state = getMortarState(); 
 
 </script>
 
 <div class='flex h-screen w-full'>
-    <div class='w-full bg-black'></div>
+    <MapViewport></MapViewport>
     <div class='justify-end items-stretch h-screen'>
         <ControlPanel></ControlPanel>
     </div>

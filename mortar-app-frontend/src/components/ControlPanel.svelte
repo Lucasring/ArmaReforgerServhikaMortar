4accf62dfd1a0299a4aab3aec1d555b892359633
Lucas Ringe <lucaswringe@gmail.com>
2026-01-17 21:42:58 +0000

    feature: Added Target Cache to ControlPanel

--- mortar-app-frontend/src/components/ControlPanel.svelte
+++ mortar-app-frontend/src/components/ControlPanel.svelte
@@ -3,6 +3,7 @@
     import { MAP_SCALE_METERS_PER_PIXEL, mortarTypes } from "$lib/mortar_config";
     import type { MortarConfig, Mortar, MortarAmmo, RingData, BallisticData} from "$lib/mortar_config_types"
     import { getMortarState, MortarState } from "$lib/mortar_state.svelte";
+    import ControlPanelCache from "./ControlPanelCache.svelte";
 
     // Get the MortarState fro mthe context
     let mortar_state : MortarState = getMortarState();
@@ -23,13 +24,13 @@
 <div class="flex flex-col gap-4 w-75 p-5 bg-stone-700 h-screen">
     
     <!-- Header -->
-    <h2 class="text-xl text-center font-bold text-white">Mortar Helper V2.3.0</h2>
+    <h2 class="text-xl text-center font-bold text-stone-300">Mortar Helper V3.0.0</h2>
 
     <!-- User Input -->
     <div class="bg-stone-600 rounded-lg p-2">
 
         <!-- Mortar Type Selector  -->
-        <div class="text-white">
+        <div class="text-stone-300">
             <label for="mortar-type" class="font-bold p-1">Mortar Type:</label>
             <select id="mortar-type" class="w-full rounded-lg bg-stone-800" bind:value={mortar_state.mortar_type}>
                 {#each Object.values(mortar_config) as mortar}
@@ -39,7 +40,7 @@
         </div>
 
         <!-- Mortar Shell Type Selector -->
-        <div class="text-white">
+        <div class="text-stone-300">
             <label for="ammo-type" class="font-bold p-1">Ammo Type:</label>
             <select id="ammo-type" class="w-full rounded-lg bg-stone-800" bind:value={mortar_state.shell_type}>
                 {#if mortar_state.mortar_type }
@@ -51,7 +52,7 @@
         </div>
 
         <!-- Mortar Ring Count Selector -->
-        <div class="text-white">
+        <div class="text-stone-300">
             <label for="ring-select" class="font-bold p-1">Ring:</label>
             <select 
                 id="ring-select" 
@@ -72,7 +73,7 @@
 
         <!-- Auto Ring Enable -->
         <div class="flex w-full items-center justify-center py-2">
-            <label class="text-white font-bold">
+            <label class="text-stone-300 font-bold">
                 <input type="checkbox" class="rounded-sm" bind:checked={mortar_auto_ring_enabled}/> Auto-select Ring
             </label>
         </div>
@@ -81,11 +82,9 @@
 
     <!-- Calculation Block -->
     <div class="bg-stone-600 p-2 rounded-lg">
-        <h3 class="text-center text-white">
-        <b class="underline">Calculations</b>
-        </h3>
+        <h3 class="text-center text-stone-300 border-b font-bold">Calculations</h3>
         {#snippet displayCalculatedMember(label : string, units : string, value : number | string | null)}
-        <div class="flex text-white">
+        <div class="flex text-stone-300">
             <span class="w-full font-bold">{label} ({units}):</span>
             {#if value}
             <span class="text-right">{value}</span>
@@ -100,8 +99,11 @@
         {@render displayCalculatedMember("Time To Impact", "s", mortar_state.target_time_to_impact?.toFixed(1) ?? 0)}
     </div>
 
+    <!-- Mortar Cache -->
+    <ControlPanelCache></ControlPanelCache>
+    
     <!-- Instruction Block -->
-    <div class="bg-stone-600 rounded-lg p-4 text-white">
+    <div class="bg-stone-600 rounded-lg p-4 text-stone-300">
         <h3 class="underline text-center font-bold text-md">Instructions</h3>
         <ul class='flex flex-col gap-2 m-3 pl-1 text-sm text-left list-disc'>
         <li class="">Click and drag to pan the map</li>

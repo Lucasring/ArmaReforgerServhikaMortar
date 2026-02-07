1e18123e8039c0fc0ca43599345ad4ad2691b95a
Lucas Ringe <lucaswringe@gmail.com>
2026-02-07 23:54:06 +0000

    feature: Adjusted Target Create and Remove routes to use new request model

--- backend/src/routers/squad.py
+++ backend/src/routers/squad.py
@@ -4,7 +4,7 @@ from sqlmodel import Session, select
 from typing import List
 
 from ..database import get_session
-from ..datamodel import SquadSession, Target, TargetCreate, User
+from ..datamodel import SquadSession, Target, TargetCreateRequest, TargetRemoveRequest, User
 
 session_key = APIKeyHeader(name='X-SESSION-KEY')
 
@@ -84,62 +84,61 @@ def get_session_targets(
     return db.exec(statement).all()
 
 
-@router.delete("/targets", response_model=Target)
-def remove_session_target(
-    target_key : int,
+@router.delete("/targets")
+def remove_session_user_target(
+    target_request: TargetRemoveRequest, 
     session_name: str = Security(session_key),
     db : Session = Depends(get_session),
 ):
-    # 1. Fetch the target
-    target = db.get(Target, target_key)
+    target_statement = (
+        select(Target)
+        .join(SquadSession)
+        .where(SquadSession.session_name == session_name)
+        .where(Target.user_id == target_request.user_id)
+    )
+
+    target = db.exec(target_statement).first()
     if not target:
         raise HTTPException(status_code=404, detail="Target not found")
     
-    # 2. Security Check: Verify this target belongs to the requester's squad
-    statement = (
-        select(SquadSession)
-        .where(SquadSession.id == target.session_id)
-        .where(SquadSession.session_name == session_name)
-    )
-
-    authorized_session = db.exec(statement).first()
-    if not authorized_session:
-        raise HTTPException(
-            status_code=403, 
-            detail="You do not have permission to delete this target"
-        )
-    
-    # 3. Delete the target
     db.delete(target)
     db.commit()
-    return target
+    return {"status" : "success"}
 
 
 @router.post("/targets", response_model=Target)
-def add_session_target(
-    base_target: TargetCreate, 
+def add_session_user_target(
+    target_request: TargetCreateRequest, 
     session_name : str = Security(session_key),
     db: Session = Depends(get_session)
 ):
-    # 1. Verify the current_session exists
+    # Verify the current_session exists
     squad_session = db.exec(select(SquadSession).where(SquadSession.session_name == session_name)).first()
     if not squad_session or squad_session.id is None:
         raise HTTPException(status_code=404, detail="Session not found")
 
-    # 2. Assign the squad_session ID to the target
-    target = Target(
-        label = base_target.label,
-        x = base_target.x,
-        y = base_target.y,
-        timestamp = base_target.timestamp,
-        session_id = squad_session.id
-    )
-    
-    # 3. Add the target to the Database
-    db.add(target)
+    # Verify the User who sent the target exists
+    target_user = db.get(User, target_request.user_id)
+    if not target_user or target_user.id is None:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    # Create or Update the Target
+    if target_user.target:
+        target_user.target.x = target_request.x
+        target_user.target.y = target_request.y
+        target = target_user.target
+    else:
+        # Create a brand new target object
+        target = Target(
+            x=target_request.x,
+            y=target_request.y,
+            user_id=target_user.id,
+            session_id=squad_session.id
+        )
+        db.add(target)
+
     db.commit()
     db.refresh(target)
-
     return target
 
 # ----- User Routes -----

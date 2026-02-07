c3c5c9bc559adfe37bdc9988dc0e1cb204c0e59a
Lucas Ringe <lucaswringe@gmail.com>
2026-02-07 20:39:31 +0000

    feature: Added User to Datamodel

--- backend/src/datamodel.py
+++ backend/src/datamodel.py
@@ -1,9 +1,11 @@
-from sqlmodel import Field, Relationship, SQLModel, create_engine, select
+from sqlmodel import Field, Relationship, SQLModel, create_engine, select, UniqueConstraint
 from pydantic import BaseModel
+from typing import Optional
 
 class SquadSession(SQLModel, table=True):
     id : int | None = Field(default=None, primary_key=True)
-    name : str = Field(unique=True, index=True)
+    session_name : str = Field(unique=True, index=True)
+    users : list["User"] = Relationship(back_populates="session")
     targets: list["Target"] = Relationship(back_populates="session")
 
 class TargetCreate(BaseModel):
@@ -19,5 +21,26 @@ class Target(SQLModel, table=True):
     y: float
     timestamp: float | None = None
     
+    # Session Connection
     session_id : int = Field(default=None, foreign_key="squadsession.id")
     session: SquadSession = Relationship(back_populates="targets")
+
+    # User Connection
+    user_id : int = Field(default=None, foreign_key="user.id", unique=True)
+    user : "User" = Relationship(back_populates="target")
+
+class User(SQLModel, table=True):
+    __table_args__ = (
+        UniqueConstraint("session_id", "name", name="unique_user_in_session"),
+    )
+
+    id : int | None = Field(default=None, primary_key=True)
+    name : str
+    is_active : bool
+
+    # Mortar Session Details
+    target : Optional["Target"] = Relationship(back_populates="user")
+
+    # Session Connection
+    session_id : int = Field(default=None, foreign_key="squadsession.id")
+    session : SquadSession = Relationship(back_populates="users")

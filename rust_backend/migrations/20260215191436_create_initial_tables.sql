d3baabb9b22a1ca25024ccb9fea9a698befa483e
Lucas Ringe <lucaswringe@gmail.com>
2026-02-18 00:29:54 +0000

    feature: added SQL table method to delete session if no users left

--- rust_backend/migrations/20260215191436_create_initial_tables.sql
+++ rust_backend/migrations/20260215191436_create_initial_tables.sql
@@ -21,4 +21,20 @@ CREATE TABLE target (
     user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
 
     CONSTRAINT unique_target_per_user UNIQUE (session_id, user_id)
-)
+);
+
+CREATE FUNCTION delete_empty_session()
+RETURNS TRIGGER AS $$
+BEGIN
+    -- Check if any users remain for the session being left
+    IF NOT EXISTS (SELECT 1 FROM users WHERE session_id = OLD.session_id) THEN
+        DELETE FROM squadsession WHERE id = OLD.session_id;
+    END IF;
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trigger_delete_empty_session
+AFTER DELETE OR UPDATE OF session_id ON users
+FOR EACH ROW
+EXECUTE FUNCTION delete_empty_session();
